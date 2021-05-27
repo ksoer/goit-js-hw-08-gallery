45fbda9ab7cbd75c8978653e06c5e5ca6584d299
ksoer <ksoerr@gmail.com>
2021-05-27 15:12:24 +0300

    HW_8 done

--- src/gallery-items.js
+++ src/gallery-items.js
@@ -1,4 +1,4 @@
-export default [
+const links = [
   {
     preview:
       'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
@@ -63,3 +63,76 @@ export default [
     description: 'Lighthouse Coast Sea',
   },
 ];
+
+const listImg = document.querySelector('.js-gallery');
+const cardsMarkup = createGaleryCard(links);
+const ligthBox = document.querySelector('.js-lightbox')
+const lightBoxImage = document.querySelector('.lightbox__image')
+const btnClose = document.querySelector('[data-action="close-lightbox"]')
+const overlay = document.querySelector('.lightbox__overlay')
+
+
+// Слухачі)
+
+listImg.insertAdjacentHTML('beforeend', cardsMarkup);
+listImg.addEventListener('click', onListImgClick)
+btnClose.addEventListener('click', onListImgClickClose)
+overlay.addEventListener('click', onListImgClickClose)
+window.addEventListener("keyup", pressKey);
+
+
+// Додавання списку картинок
+
+function createGaleryCard(links) {
+  return links
+  .map(({ preview, original, description }) => {
+    return `
+    <li class="gallery__item">
+    <a
+      class="gallery__link"
+      href="${original}"
+    >
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>
+  `;
+  }).join('');
+} 
+
+
+// Відкриття модального вікна 
+
+function onListImgClick(evt) {
+  evt.preventDefault();
+  ligthBox.classList.add('is-open')
+  lightBoxImage.src = evt.target.getAttribute("data-source");
+  lightBoxImage.alt = evt.target.alt;
+}
+
+
+// Закриття модального вікна 
+
+function onListImgClickClose(evt) {
+  ligthBox.classList.remove('is-open')
+  lightBoxImage.src = " ";
+  lightBoxImage.alt = " ";
+} 
+
+
+
+function pressKey(evt){
+  if(evt.code === "Escape") {
+  onListImgClickClose()
+  }
+  if(evt.code === "ArrowRight"){
+
+  }
+  if(evt.code === "ArrowLeft"){
+
+  }
+}
